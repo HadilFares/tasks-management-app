b398863e54172c3f5facb715e1c3d57163aa89b6
hadil30 <hedilfares30@gmail.com>
2021-09-06 11:57:18 +0400

    export to excel

--- src/components/FilterBar.js
+++ src/components/FilterBar.js
@@ -1,60 +1,64 @@
-import {useState} from "react";
-const FilterBar =({
+import { useState ,useEffect} from "react";
+import axios from "axios";
+import { Form, Button } from "react-bootstrap";
 
-    onDateFilter,
-})=>{
-    const [filters,setFilters]=useState({
-        
-        from:"",
-        to:""
-    });
-    const handleInput =(field)=>(event)=>{
-        const {value}=event.target;
-        setFilters({
-            ...filters,
-            [field]:value,
-        });
-        switch (field){
-            
-            case"from":
-                onDateFilter(value,"from");
-            break;
-            case "to":
-                onDateFilter(value,"to");
-            break;
-            default:
-            break;
-        }
-    };
-    return (
-        <div className="row my-5">
-        <div className="col">
-          <h4 className="border-bottom">Filters</h4>
-        </div>
-        
+const FilterBar = ( ) => {
+  const [fromDate, setDate1] = useState();
+  const [toDate, setDate2] = useState();
+  const [data, setData] = useState();
 
+  const filteredUser = async (date1, date2) => {
+    await axios
+      .get(`http://localhost:3000/search/${date1}/${date2}`)
+      .then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      });
+  };
+  
+  useEffect(() => {
+    const fetchData = () =>{
+     axios.get('http://localhost:3000/read').then(r => setData(r.data) )
+    }
+    fetchData()
+  }, [])
+  
 
+  return (
+    <div className="row my-5">
+      <div className="col">
+        <h4 className="border-bottom">Filters</h4>
+      </div>
+      <Form onSubmit={() => filteredUser(fromDate, toDate)}>
         <div className="col-sm-12 my-2">
-        <label htmlFor="startDate">From</label>
-        <input
-          type="date"
-         
-          className="form-control"
-          id="startDate"
-          onChange={handleInput("from")}
-        />
-      </div>
-      <div className="col-sm-12 my-2">
-        <label htmlFor="endDate">To</label>
-        <input
-          type="date"
-         
-          className="form-control"
-          id="endDate"
-          onChange={handleInput("to")}
-        />
-      </div>
-  </div>
-    );
+          <label htmlFor="startDate">From</label>
+          <input
+            type="date"
+            name="date1"
+            className="form-control"
+            onChange={(e) => setDate1(e.target.value)}
+          />
+        </div>
+        <div className="col-sm-12 my-2">
+          <label htmlFor="endDate">To</label>
+          <input
+            type="date"
+            name="date2"
+            className="form-control"
+            onChange={(e) => setDate2(e.target.value)}
+          />
+        </div>
+        {/*<button type="submit" onClick={()=>filteredUser(fromDate, toDate)}>submit</button>*/}
+        <div className="text-center">
+          <Button
+            variant="primary"
+            onClick={() => filteredUser(fromDate, toDate)}
+          >
+            Search
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
 };
 export default FilterBar;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,117 +1,160 @@
-import React, { useState, useEffect} from "react";
+import { Form, Button } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+//import ReactToExcel from "react-html-table-to-excel";
+import ReactExport from "react-data-export";
 
+import moment from "moment";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import { ExportToExcel } from "./ExportToExcel";
 
-import dayjs from "dayjs";
-import FilterBar from "./FilterBar";
-const isSameOrAfter = require("dayjs/plugin/isSameOrAfter");
-const isSameOrBefore = require("dayjs/plugin/isSameOrBefore");
-dayjs.extend(isSameOrBefore);
-dayjs.extend(isSameOrAfter);
-
+const ExcelFile = ReactExport.ExcelFile;
+const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
+const ExcelColumn = ReactExport.ExcelFile.ExcelColumn;
 
 const Home = () => {
   const [userList, setuserList] = useState([]);
-  const[AllData,setAllData]=useState([]);
-  const [data, setData] = React.useState([])
-  const fileName = "myfile"; // here enter filename for your excel file
+  //const [data, setData] = useState([]);
+  const [getUserList , setGetUserList] = useState([]);
+  const [fromDate, setDate1] = useState();
+  const [toDate, setDate2] = useState();
 
-  useEffect(() => {
-    const fetchData = () =>{
-     axios.get('http://localhost:3000/read').then(r => setData(r.data) )
+  const filteredUser = async (date1, date2) => {
+    if (typeof date1 != "undefined" || typeof date2 != "undefined") {
+      await axios
+        .get(`http://localhost:3000/search/${date1}/${date2}`)
+        .then((response) => {
+          //setuserList(response.data);
+          let object = response.data;
+          setGetUserList(object)
+          let date = [{date1 : date1 ,date2 : date2}]
+          setuserList([...object , ...date])
+          console.log(userList)
+        });
+    } else {
+      const result = await axios.get("http://localhost:3000/read");
+      setuserList(result.data.reverse());
     }
-    fetchData()
-  }, [])
-  
-  useEffect(() => {
-    
-   loadUsers();
-  }, []);
-
-  
-  
-  const handleFilterDate = (date, field) => {
-    const filteredData = userList.filter((item) => {
-      if (field === "from" && dayjs(item.dateDemarrage).isSameOrAfter(dayjs(date))) {
-        return item ;
-      }
-     });   
-    setAllData(filteredData);
   };
 
-  const loadUsers = async() =>{
-    const result=await axios.get('http://localhost:3000/read');
+  /*
+  useEffect(() => {
+    const fetchData = () => {
+      axios.get("http://localhost:3000/read").then((r) => setData(r.data));
+    };
+    fetchData();
+  }, [],[data]);
+*/
+  useEffect(
+    () => {
+      filteredUser();
+    },
+    [],
+    [userList]
+  );
+
+  const loadUsers = async () => {
+    const result = await axios.get("http://localhost:3000/read");
     setuserList(result.data.reverse());
   };
-  const deleteUser = async id =>{
+
+  const deleteUser = async (id) => {
     await axios.delete(`http://localhost:3000/delete/${id}`);
     loadUsers();
   };
 
-
- 
   return (
-    <div className="container">
-      <div className="py-4">
-        <h1>Home Page</h1>
-        <div className="col-lg-9 mt -2 mb-2">
-         
-         
-          <FilterBar 
-          onDateFilter={handleFilterDate}
-          
-          />
-          <ExportToExcel apiData={data} fileName={fileName} />
-        
-       
+    <>
+      <div className="row my-5">
+        <div className="col">
+          <h4 className="border-bottom">Filters</h4>
         </div>
-        <table id="tblData" className="table border shadow">
-          <thead className="thead-dark">
-            <tr>
-              <th scope="col">#</th>
-          
-              <th scope="col">Matricule</th>
-              <th scope="col">Name</th>
-              <th scope="col">LastName</th>
-              <th scope="col">DateDemarrage</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-        
-            {AllData.map((user,id) => (
-              <tr key={id}>
-               <th scope="row"></th>
-                
-                <td>{user.matricule}</td>
-                <td>{user.name}</td>
-                <td>{user.lastname}</td>
-                <td>{dayjs(user?.dateDemarrage).format("YYYY / MM / DD")}</td>
-                <td>
-                
-  
-                  <Link
-                    className="btn btn-outline-primary mr-2"
-                    to={`/users/edit/${user._id}`}
-                  >
-                    Edit
-                  </Link>
-                  <Link
-                    className="btn btn-danger"
-                    onClick={() => deleteUser(user._id)}
-                  >
-                    Delete
-                  </Link>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      
+        <Form onSubmit={() => filteredUser(fromDate, toDate)}>
+          <div className="col-sm-12 my-2">
+            <label htmlFor="startDate">From</label>
+            <input
+              type="date"
+              name="date1"
+              className="form-control"
+              onChange={(e) => setDate1(e.target.value)}
+            />
+          </div>
+          <div className="col-sm-12 my-2">
+            <label htmlFor="endDate">To</label>
+            <input
+              type="date"
+              name="date2"
+              className="form-control"
+              onChange={(e) => setDate2(e.target.value)}
+            />
+          </div>
+          {/*<button type="submit" onClick={()=>filteredUser(fromDate, toDate)}>submit</button>*/}
+          <div className="text-center">
+            <Button
+              variant="primary"
+              onClick={() => filteredUser(fromDate, toDate)}
+            >
+              Search
+            </Button>
+          </div>
+        </Form>
       </div>
-    </div>
+      <div className="container">
+        <div className="py-4">
+          <h1>Home Page</h1>
+
+          <table id="tblData" className="table border shadow">
+            <thead className="thead-dark">
+              <tr>
+                <th scope="col">#</th>
+
+                <th scope="col">User_ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">LastName</th>
+                <th scope="col">hiring date</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {getUserList.map((user, id) => (
+                <tr key={id}>
+                  <th scope="row"></th>
+                  <td>{user.matricule}</td>
+                  <td>{user.name}</td>
+                  <td>{user.lastname}</td>
+                  <td>
+                    {moment(user?.dateDemarrage).format("YYYY / MM / DD")}
+                  </td>
+                  <td>
+                    <Link
+                      className="btn btn-outline-primary mr-2"
+                      to={`/users/edit/${user._id}`}
+                    >
+                      Edit
+                    </Link>
+                    <Link
+                      className="btn btn-danger"
+                      onClick={() => deleteUser(user._id)}
+                    >
+                      Delete
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <ExcelFile filename={"ExcelExport"} element={<button>Export</button>}>
+            <ExcelSheet data={userList} name="userList">          
+              <ExcelColumn label="User_ID" value="matricule" />
+              <ExcelColumn label="Name" value="name" />
+              <ExcelColumn label="LastName" value="lastname" />
+              <ExcelColumn label="hiring date" value="dateDemarrage" />
+              <ExcelColumn label="fromDate" value="date1" />
+              <ExcelColumn label="toDate" value="date2" />
+            </ExcelSheet>
+          </ExcelFile>
+        </div>
+      </div>
+    </>
   );
 };
 
